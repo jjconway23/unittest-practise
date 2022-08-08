8a7f35a07ccbae453f4adeac3e337a59f9af6d9a
jjconway23 <jacob.peat01@gmail.com>
2022-08-08 11:56:09 +0000

    Define variables within the Student class

---
+++ student.py
@@ -0,0 +1,11 @@
+from datetime import date,timedelta
+
+class Student:
+    """A Student Class as base for method testing"""
+
+    def __init__(self, first_name, last_name):
+        self._first_name = first_name
+        self._last_name = last_name
+        self._start_date = date.today()
+        self._end_date = date.today() + timedelta(days=365)
+        self._naughty_list = False
